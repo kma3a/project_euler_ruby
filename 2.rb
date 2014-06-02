47480560b047975c8dfd8cddba07a90e7478683f
Kelsey <kma3a@virginia.edu>
2014-06-02 16:27:56 -0500

    solved problem 2 both recursive and not

--- 2.rb
+++ 2.rb
@@ -1,1 +1,27 @@
-2.rb
+#recursive
+
+def fib_1(start_num, end_num, array)
+	while end_num < 4000000
+		new_num = start_num + end_num
+		array << end_num if end_num % 2 == 0
+		fib_1(end_num, new_num, array)
+		return array.inject(&:+)
+	end
+end
+
+p fib_1(0,1,[])
+
+# not recursive
+
+def fib_2(start_num, end_num)
+	array = []
+	while end_num < 4000000
+		new_num = start_num + end_num
+		start_num = end_num
+		end_num = new_num
+		array << end_num if end_num % 2 == 0
+	end
+	puts array.inject(&:+)
+end
+
+fib_2(0,1)
